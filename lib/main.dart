920032b966da00d88b0689bbcfb0a6714d707084
mac <zayns1914@gmail.com>
2023-01-09 19:36:37 +0200

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -155,6 +155,7 @@ class _HomePageState extends State<HomePage> {
         brightness: Brightness.dark,
       ),
       body: Column(
+
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           const Text(
